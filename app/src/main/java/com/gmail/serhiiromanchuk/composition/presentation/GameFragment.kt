cbc34fa870910f65c0692cda3e3ab825aa934e6b
Romanchuk Serhii <serhiiromanchuk@gmail.com>
2022-07-15 14:10:54 +0300

    Create Presentation

--- app/src/main/java/com/gmail/serhiiromanchuk/composition/presentation/GameFragment.kt
+++ app/src/main/java/com/gmail/serhiiromanchuk/composition/presentation/GameFragment.kt
@@ -8,14 +8,21 @@ import androidx.fragment.app.Fragment
 import com.gmail.serhiiromanchuk.composition.databinding.FragmentGameBinding
 
 class GameFragment : Fragment() {
-    private lateinit var binding: FragmentGameBinding
+    private var _binding: FragmentGameBinding? = null
+    private val binding: FragmentGameBinding
+        get() = _binding ?: throw RuntimeException("FragmentGameBinding == null")
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        binding = FragmentGameBinding.inflate(inflater, container, false)
+    ): View {
+        _binding = FragmentGameBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
