4790fa68f6f20aeaa3cc9baac869d054d2be6228
Romanchuk Serhii <serhiiromanchuk@gmail.com>
2022-07-25 16:13:09 +0300

    Add Navigation Jetpack

--- app/src/main/java/com/gmail/serhiiromanchuk/composition/presentation/WelcomeFragment.kt
+++ app/src/main/java/com/gmail/serhiiromanchuk/composition/presentation/WelcomeFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.gmail.serhiiromanchuk.composition.R
 import com.gmail.serhiiromanchuk.composition.databinding.FragmentWelcomeBinding
 
@@ -30,10 +31,7 @@ class WelcomeFragment : Fragment() {
     }
 
     private fun launchChooseLevelFragment() {
-        requireActivity().supportFragmentManager.beginTransaction()
-            .replace(R.id.main_fragment, ChooseLevelFragment.newInstance())
-            .addToBackStack(null)
-            .commit()
+        findNavController().navigate(R.id.action_welcomeFragment_to_chooseLevelFragment)
     }
 
     override fun onDestroyView() {
